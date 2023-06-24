e63e41a3d6aa3292f7d8f4f39925000e37a4534f
John <wilmervalente5@gmail.com>
2023-06-23 23:27:16 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import NavBar from "./NavBar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>My First React </h1>
+    <h1>My First React App</h1>
 
     <Router>
       <NavBar />
