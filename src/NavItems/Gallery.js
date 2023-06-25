7e53fb9f3a9f8e7bb4d5b20e7131d602e073733c
John <wilmervalente5@gmail.com>
2023-06-25 00:14:34 -0500

    modified img

--- src/NavItems/Gallery.js
+++ src/NavItems/Gallery.js
@@ -9,136 +9,136 @@ const Gallerry = () => {
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20161102-P1040350.jpg'
+            src='https://estilonext.com/_next/image?url=https:%2F%2Feststatic.com%2F1043%2F0.89799700-1536731694MJ2yRkafjhin8z7C.jpg&w=1280&q=75'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20170126-P1000299.jpg'
+            src='https://images8.alphacoders.com/108/thumbbig-1089731.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20170722-_1100175.jpg'
+            src='https://www.taiwangoodlife.org/files/imagecache/photo_500x375/photo/Dan_Shui_He_Ri_Luo_.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20170722-_1100265.jpg'
+            src='https://www.gannett-cdn.com/-mm-/68f548c1c9118861b2b3d23b7d32712e191d2a69/c=420-0-3745-2500&r=x513&c=680x510/local/-/media/2018/04/14/USATODAY/USATODAY/636592630503865415-GettyImages-538154034.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20170805-_1100655.jpg'
+            src='https://enigmasandesperu.com/wp-content/uploads/2019/04/enigmas-andes-peru-tour-llacta-machu-picchu-1024x791.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20170828-P1100756.jpg'
+            src='https://cdn.quizly.co/wp-content/uploads/2019/05/01010338/7226208f8a43a810dfe566cc29a14727-300x300.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20171119-_1110070-2.jpg'
+            src='https://1.bp.blogspot.com/-tqiRRV1N0lY/XzrEgnf9QEI/AAAAAAAAAAg/J0BIiRMMJ4wdz-IcZF8CXPEJeLKevEIhgCLcBGAsYHQ/s1024/ciudad2.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20171119-_1110112.jpg'
+            src='https://64.media.tumblr.com/52cc5a2f3a9d63e3f5dc31890eac1d96/tumblr_pce4sdd1M11wyd6eeo3_640.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20171119-_1110143.jpg'
+            src='https://i.pinimg.com/736x/4b/1a/00/4b1a00262b0775beeb44e71aa20a47f1.jpg'
           />
         </div>
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20171219-_1110676.jpg'
-          />
-        </div>
-
-        <div className='rowImage'>
-          <img
-            className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20180118-_1130419.jpg'
-          />
-        </div>
-        <div className='rowImage'>
-          <img
-            className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20180119-_1130670.jpg'
-          />
-        </div>
-        <div className='rowImage'>
-          <img
-            className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/20180720-P1030318.jpg'
+            src='https://i.pinimg.com/474x/2b/5e/80/2b5e80f0786df0b55b63163302c672a5.jpg'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1386.jpg'
+            src='https://images.lonelyplanetitalia.it/uploads/shutterstockrf-306582686?q=80&p=social&s=3d24587837039c490793ebbb4f4c9029https://images.lonelyplanetitalia.it/uploads/shutterstockrf-306582686?q=80&p=social&s=3d24587837039c490793ebbb4f4c9029'
+          />
+        </div>
+        <div className='rowImage'>
+          <img
+            className='smallImage'
+            src='https://www.wikihow.com/images_es/thumb/2/2a/Imagen1.jpg/-crop-375-321-375px-Imagen1.jpg'
+          />
+        </div>
+        <div className='rowImage'>
+          <img
+            className='smallImage'
+            src='https://enviajes.cl/wp-content/uploads/2017/11/Viajes-a-Ecuador-Napo.jpg'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1388.jpg'
+            src='https://th.bing.com/th/id/R.c3466918220338c8f54a088179ab9895?rik=NkwKon6yxVjklg&riu=http%3a%2f%2f1.bp.blogspot.com%2f_fRTNKl3vwm8%2fS1_0xbNqvPI%2fAAAAAAAAAlo%2fZKEi7tLihpI%2fw1200-h630-p-k-no-nu%2fGalapagos%2bIslands%2bTravel%2bGuide.jpg&ehk=cxm76RsABcyMBsCDJuhBnyZJKCGPFti8SIym%2bjrcMDk%3d&risl=&pid=ImgRaw&r=0'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1393.jpg'
+            src='https://www.beytoote.com/images/stories/iran/hhi2392.jpg'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1404.jpg'
+            src='https://ailovei.com/wp-content/uploads/2015/07/%E7%B5%B6%E6%99%AF-55.jpg'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1514.jpg'
+            src='https://th.bing.com/th/id/R.b2658dcbe025938d5f8fe41ab5b8c3e1?rik=c67WGb3gT1H3MQ&riu=http%3a%2f%2fyourguidetoitaly.com%2fslowitaly%2fwp-content%2fuploads%2f2012%2f07%2fragusa-ibla-san-giuseppe-2-300x417.jpg&ehk=HVBn3ERctkXqAJY4oxXhgHl5jqjQzyxereCXt7pmnyc%3d&risl=&pid=ImgRaw&r=0'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1540.jpg'
+            src='https://i.pinimg.com/736x/59/01/41/590141b848fed56715caf3e3804aaaca.jpg'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1565.jpg'
+            src='https://th.bing.com/th/id/R.32ec9a2f42e8c872b6c0cba226d5be6d?rik=itryI9MBlQEMfw&pid=ImgRaw&r=0'
           />
         </div>
 
         <div className='rowImage'>
           <img
             className='smallImage'
-            src='https://sebasdm7.github.io/WEBSITE/WebsitePhotos/img/DSCF1573%201.jpg'
+            src='https://i.pinimg.com/originals/1b/85/eb/1b85eb4b1f6ca990cf3b8816b3b255c3.jpg'
+          />
+        </div>
+
+        <div className='rowImage'>
+          <img
+            className='smallImage'
+            src='https://live.staticflickr.com/5545/9099162807_02a56edb1c.jpg'
           />
         </div>
       </div>
